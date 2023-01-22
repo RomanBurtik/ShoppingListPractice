1d3ea6de645a18a1c1b15d99eaa3aae66074892d
RomanBurtik <bromav33@gmail.com>
2023-01-22 17:52:14 +0100

    Creation of UseCases

---
+++ app/src/main/java/com/example/shoppinglistpractice/domain/AddShopItemUseCase.kt
@@ -0,0 +1,4 @@
+package com.example.shoppinglistpractice.domain
+
+class AddShopItemUseCase {
+}
---
+++ app/src/main/java/com/example/shoppinglistpractice/domain/ShopItem.kt
@@ -0,0 +1,4 @@
+package com.example.shoppinglistpractice.domain
+
+data class ShopItem(val id: Int, val text: String, val count: Int, val isActive: Boolean) {
+}
